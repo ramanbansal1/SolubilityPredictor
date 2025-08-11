33c5ea4db1093a1aabfbc965150b307b6e0d3a70
Raman Bansal <bansalraman339@gmail.com>
2025-08-11 08:31:46 +0530

    Update

--- data/raw.py
+++
@@ -1,7 +0,0 @@
-import os 
-import requests
-import pandas as pd
-
-url = "https://raw.githubusercontent.com/ESAIBMCIC/MolecularSolubilityPrediction/master/delaney.csv"
-df = pd.read_csv(url)
-print(df.head())
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from chemicalpropertypredictor!")
+
+
+if __name__ == "__main__":
+    main()
